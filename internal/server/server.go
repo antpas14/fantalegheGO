fd42f74779d6d3fef5f0421e0243c8e194de0075
antpas14 <{ID}+{username}@users.noreply.github.com>
2023-10-08 17:31:18 +0100

    fixing using sync map and bug fixing

--- internal/server/server.go
+++ internal/server/server.go
@@ -13,6 +13,10 @@ import (
 type MyServer struct{}
 
 func (s *MyServer) Calculate(ctx echo.Context, params api.CalculateParams) error {
+    if params.LeagueName == nil {
+		// Handle the case where LeagueName is nil, perhaps return an error.
+		return ctx.JSON(http.StatusBadRequest, "LeagueName is required")
+	}
 
 	// Get ranks from calculate module
 	ranks := calculate.GetRanks(*params.LeagueName)
