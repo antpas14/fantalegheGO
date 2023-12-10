0e69cc47a8839381517b396e4bb004fd2776f83b
antpas14 <{ID}+{username}@users.noreply.github.com>
2023-12-10 18:48:52 +0000

    adding tests

--- internal/calculate/calculate.go
+++ internal/calculate/calculate.go
@@ -10,29 +10,29 @@ import (
     "fmt"
     "log"
     "sync"
-
 )
 
+// Create fetcher and parser instances at the module level
+var configInstance, _ = config.LoadConfig()
+var parserInstance = &parser.ParserImpl{}
+var fetcherInstance = &fetcher.FetcherImpl{configInstance.FetcherUrl}
+
 // GetRanks retrieves a list of ranks (api.Rank)
-func GetRanks(leagueName string) []api.Rank {
-    config, err := config.LoadConfig();
-    if err != nil {
-        log.Fatal(err)
-    }
+func GetRanks(leagueName string, parserInstance parser.Parser) []api.Rank {
+    fmt.Printf("parser instance is %v", parserInstance)
+	// Retrieve raw data using fetcher
+	rankingsRaw, _ := fetcherInstance.Retrieve(configInstance.BaseUrl + leagueName + configInstance.RankingUrl)
+	calendarRaw, _ := fetcherInstance.Retrieve(configInstance.BaseUrl + leagueName + configInstance.CalendarUrl)
+//     fmt.Printf("RESULTS_RAW IS %v", calendarRaw)
 
-	parser := &parser.ParserImpl{}
-    fetcher := &fetcher.FetcherImpl{config.FetcherUrl}
-    // Retrieve raw data using fetcher
-    rankingsRaw, _ := fetcher.Retrieve(config.BaseUrl + leagueName + config.RankingUrl)
-    calendarRaw, _ := fetcher.Retrieve(config.BaseUrl + leagueName + config.CalendarUrl)
+	rankings, err := parserInstance.GetPoints(rankingsRaw)
+	results, err := parserInstance.GetResults(calendarRaw)
+    fmt.Printf("RESULTS IS %v", results)
 
-
-    rankings, err := parser.GetPoints(rankingsRaw)
-    results, err := parser.GetResults(calendarRaw)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("LeagueName is %s", leagueName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("LeagueName is %s", leagueName)
 
 	// Perform data retrieval and processing here
 	// For this example, we'll return a static list of ranks
@@ -40,62 +40,67 @@ func GetRanks(leagueName string) []api.Rank {
 }
 
 func calculate(rankings map[string]int, results *sync.Map) []api.Rank {
-    var evPointsMap = make(map[string]float64)
+	var evPointsMap = make(map[string]float64)
 
-    for teamName,_ := range rankings {
-        evPointsMap[teamName] = 0
-    }
-    var combinations = float64(len(rankings) - 1)
-    var teamResults []parser.TeamResult
+	for teamName, _ := range rankings {
+		evPointsMap[teamName] = 0
+	}
+	var combinations = float64(len(rankings) - 1)
+	var teamResults []parser.TeamResult
+    fmt.Printf("RESULTS IS %v", results)
 
-    results.Range(func(_, value interface{}) bool {
-        teamResults = make([]parser.TeamResult, 0)
-        if trSlice, ok := value.([]parser.TeamResult); ok {
-            teamResults = append(teamResults, trSlice...)
-        }
-        for i, teamResult1 := range teamResults {
-            if teamResult1.Points == -1 {
-                break;
-            }
-            var expectedPointForTeamForMatch float64
-            var pointsForAllCombinations float64
+	results.Range(func(_, value interface{}) bool {
+		teamResults = make([]parser.TeamResult, 0)
 
-            for j, teamResult2 := range teamResults {
-                if (i != j) {
-                    pointsForAllCombinations = pointsForAllCombinations + calculatePoints(teamResult1, teamResult2)
-                }
-            }
-            expectedPointForTeamForMatch = pointsForAllCombinations / combinations
-            evPointsMap[teamResult1.Name] = evPointsMap[teamResult1.Name] + expectedPointForTeamForMatch
-        }
-        return true;
-    })
-    listRank := make([]api.Rank, 0)
-    for teamName, teamEVPoints := range evPointsMap {
-        points := rankings[teamName]
 
-        // Create new variables for teamName and evPoints inside the loop
-        teamNameCopy := teamName
-        evPointsCopy := teamEVPoints
+		if trSlice, ok := value.([]parser.TeamResult); ok {
+			teamResults = append(teamResults, trSlice...)
+		}
+		for i, teamResult1 := range teamResults {
+		    fmt.Printf("TR IS %v", teamResult1)
+			if teamResult1.Points == -1 {
+				break
+			}
+			var expectedPointForTeamForMatch float64
+			var pointsForAllCombinations float64
 
-        rank := api.Rank{
-            Team:     &teamNameCopy,
-            EvPoints: &evPointsCopy,
-            Points:   &points,
-        }
-        listRank = append(listRank, rank)
-    }
-    return listRank
+			for j, teamResult2 := range teamResults {
+				if i != j {
+					pointsForAllCombinations = pointsForAllCombinations + calculatePoints(teamResult1, teamResult2)
+				}
+			}
+			expectedPointForTeamForMatch = pointsForAllCombinations / combinations
+			evPointsMap[teamResult1.Name] = evPointsMap[teamResult1.Name] + expectedPointForTeamForMatch
+		}
+		return true
+	})
+	listRank := make([]api.Rank, 0)
+	for teamName, teamEVPoints := range evPointsMap {
+		points := rankings[teamName]
+
+		// Create new variables for teamName and evPoints inside the loop
+		teamNameCopy := teamName
+		evPointsCopy := teamEVPoints
+
+		rank := api.Rank{
+			Team:     &teamNameCopy,
+			EvPoints: &evPointsCopy,
+			Points:   &points,
+		}
+		listRank = append(listRank, rank)
+	}
+	fmt.Printf("list rank is %v", listRank)
+	return listRank
 }
 
-func calculatePoints (t1 parser.TeamResult, t2 parser.TeamResult) float64 {
-    if (t1.Points > t2.Points) {
-        return 3;
-    } else if (t1.Points < t2.Points) {
-        return 0;
-    } else {
-        return 1;
-    }
+func calculatePoints(t1 parser.TeamResult, t2 parser.TeamResult) float64 {
+	if t1.Points > t2.Points {
+		return 3
+	} else if t1.Points < t2.Points {
+		return 0
+	} else {
+		return 1
+	}
 }
 
 // Helper function to create a float64 pointer
